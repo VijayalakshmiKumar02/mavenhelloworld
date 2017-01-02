fd3901d371c60aec46af963caf438d4727b1bd42
VijayalakshmiKumar02 <Vijayalakshmi.K4@cognizant.com>
2017-01-02 12:06:57 +0530

    Create build.sh

---
+++ ci/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+git clone mavenhelloworld resource-app
+echo "creating file"
+cd resource-app
+echo "created the file"
+#mvn clean compile
+#echo " done compiling"
+mvn install
+echo "done installing"
+#java de/bitkings/nitram509/ConcourseJavaMavenTestPrjApplication
+mvn test
+echo "done testing"
